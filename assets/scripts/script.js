3bd5a37cf9968dd61731c2b7ce360b5c9dc4d933
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-08-23 19:14:07 +0000

    Add query selectors to continue/quiz button classes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,38 @@
-// Global variables.
+// Global variables which remain constant.
 
 const startButton = document.querySelector('.start-button');
+
 const popupScreen = document.querySelector('.popup-screen');
+
 const exitButton = document.querySelector('.exit-button');
+
 const main = document.querySelector('.background');
 
-// Onclick event which adds the live class to the popup-screen css selector
+const continueButton = document.querySelector('.continue-button')
+
+const quizSection = document.querySelector('.quiz-section')
+
+// Onclick event (arrow function (shorthand notation for defining functions))
+// which adds the live class to the popup-screen css selector.
 startButton.onclick = () => {
     popupScreen.classList.add('live');
-    // Adds the live class to the background css selector
+
+    // Adds the live class to the background css selector.
     main.classList.add('live');
 }
 
-// Onclick event which removes the live class from the exit button css selector
+// Onclick event (arrow function (shorthand notation for defining functions))
+// which removes the live class from the exit button css selector. 
 exitButton.onclick = () => {
     popupScreen.classList.remove('live');
-    // Removes the live class from the background css selector
+
+    // Removes the live class from the background css selector.
+    main.classList.remove('live');
+}
+
+// Onclick event which adds the live class to the popup-screen css selector.
+continueButton.onclick = () => {
+    popupScreen.classList.add('live');
+    popupScreen.classList.remove('live');
     main.classList.remove('live');
 }
