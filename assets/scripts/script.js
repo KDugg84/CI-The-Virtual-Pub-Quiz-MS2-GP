ee05e8aef3477a114793255461008b3510cf7cfa
KDugg84 Duggan <kieron.duggan25@hotmail.com>
2023-08-28 21:24:34 +0000

    Replace single quote class names with double quotes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,52 +4,52 @@ import questions from "./quiz-questions.js";
 
 // Global variables which remain constant.
 
-const startButton = document.querySelector('.start-button');
+const startButton = document.querySelector(".start-button");
 
-const popupScreen = document.querySelector('.popup-screen');
+const popupScreen = document.querySelector(".popup-screen");
 
-const exitButton = document.querySelector('.exit-button');
+const exitButton = document.querySelector(".exit-button");
 
-const main = document.querySelector('.background');
+const main = document.querySelector(".background");
 
-const continueButton = document.querySelector('.continue-button');
+const continueButton = document.querySelector(".continue-button");
 
-const quizSection = document.querySelector('.quiz-section');
+const quizSection = document.querySelector(".quiz-section");
 
-const quizBox = document.querySelector('.quiz-box');
+const quizBox = document.querySelector(".quiz-box");
 
-const nextButton = document.querySelector('.next-button');
+const nextButton = document.querySelector(".next-button");
 
-const resultsBox = document.querySelector('.results-box');
+const resultsBox = document.querySelector(".results-box");
 
-const tryAgainButton= document.querySelector('.try-again-button');
+const tryAgainButton= document.querySelector(".try-again-button");
 
-const returnHomeButton= document.querySelector('.return-home-button');
+const returnHomeButton= document.querySelector(".return-home-button");
 
 // Onclick event (arrow function (shorthand notation for defining functions))
 // which adds the live class to the popup-screen css selector.
 startButton.onclick = () => {
-    popupScreen.classList.add('live');
+    popupScreen.classList.add("live");
 
     // Adds the live class to the background css selector.
-    main.classList.add('live');
+    main.classList.add("live");
 }
 
 // Onclick event (arrow function (shorthand notation for defining functions))
 // which removes the live class from the exit button css selector. 
 exitButton.onclick = () => {
-    popupScreen.classList.remove('live');
+    popupScreen.classList.remove("live");
 
     // Removes the live class from the background css selector.
-    main.classList.remove('live');
+    main.classList.remove("live");
 }
 
 // Onclick event which adds the live class to the popup-screen css selector.
 continueButton.onclick = () => {
-    quizSection.classList.add('live');
-    popupScreen.classList.remove('live');
-    main.classList.remove('live');
-    quizBox.classList.add('live');
+    quizSection.classList.add("live");
+    popupScreen.classList.remove("live");
+    main.classList.remove("live");
+    quizBox.classList.add("live");
 
     showQuestions(0);
 
@@ -59,9 +59,9 @@ continueButton.onclick = () => {
 }
 
 tryAgainButton.onclick = () => {
-    quizBox.classList.add('live');
-    nextButton.classList.remove('live');
-    resultsBox.classList.remove('live');
+    quizBox.classList.add("live");
+    nextButton.classList.remove("live");
+    resultsBox.classList.remove("live");
 
     questionCount = 0;
     questionNumber = 1;
@@ -73,9 +73,9 @@ tryAgainButton.onclick = () => {
 }
 
 returnHomeButton.onclick = () => {
-    quizSection.classList.remove('live');
-    nextButton.classList.remove('live');
-    resultsBox.classList.remove('live');
+    quizSection.classList.remove("live");
+    nextButton.classList.remove("live");
+    resultsBox.classList.remove("live");
 
     questionCount = 0;
     questionNumber = 1;
@@ -99,19 +99,19 @@ nextButton.onclick = () => {
 
         questionCounter(questionNumber);
 
-        nextButton.classList.remove('live');
+        nextButton.classList.remove("live");
     } 
     else {
         displayResultsBox();
     }
 }
 
-const answerList = document.querySelector('.answer-list');
+const answerList = document.querySelector(".answer-list");
 
 // Getting questions and options from the array 
 
 function showQuestions(index) {
-    const questionText = document.querySelector('.quiz-questions');
+    const questionText = document.querySelector(".quiz-questions");
     questionText.textContent = `${questions[index].number}. ${questions[index].question}`;
 
     let answerTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
@@ -121,9 +121,9 @@ function showQuestions(index) {
 
     answerList.innerHTML = answerTag;
 
-    const answerOptions = document.querySelectorAll('.option');
+    const answerOptions = document.querySelectorAll(".option");
     for (let i = 0; i < answerOptions.length; i++) {
-        answerOptions[i].addEventListener('click', () => optionSelected(answerOptions[i]));
+        answerOptions[i].addEventListener("click", () => optionSelected(answerOptions[i]));
     }                                     
 }
 
@@ -133,18 +133,18 @@ function optionSelected(answer) {
     let totalAnswers = answerList.children.length;
 
     if (userAnswer == correctAnswer) {
-        answer.classList.add('correct');
+        answer.classList.add("correct");
         totalScore += 1;
         headerScore();
     }
     else {
-        answer.classList.add('incorrect');
+        answer.classList.add("incorrect");
 
         // If the answer is incorrect, auto select the correct answer
 
         for (let i = 0; i < totalAnswers; i++) {
             if (answerList.children[i].textContent == correctAnswer) {
-                answerList.children[i].setAttribute('class', 'option correct');
+                answerList.children[i].setAttribute("class", "option correct");
             }
         }
     }
@@ -152,26 +152,26 @@ function optionSelected(answer) {
     // If user has selected an answer, disable all other options
 
     for (let i = 0; i < totalAnswers; i++) {
-        answerList.children[i].classList.add('.disable-answer');
+        answerList.children[i].classList.add(".disable-answer");
     }
 
-    nextButton.classList.add('live');
+    nextButton.classList.add("live");
 } 
 
 function questionCounter(index) {
-    const questionTotal = document.querySelector('.question-total');
+    const questionTotal = document.querySelector(".question-total");
     questionTotal.textContent = `${index} of ${questions.length} Questions`;
 }
 
 function headerScore() {
-    const headerScoreText = document.querySelector('.header-score');
+    const headerScoreText = document.querySelector(".header-score");
     headerScoreText.textContent = `Score: ${totalScore} / ${questions.length}`;
 }
 
 function displayResultsBox() {
-    quizBox.classList.remove('live');
-    resultsBox.classList.add('live');
+    quizBox.classList.remove("live");
+    resultsBox.classList.add("live");
 
-    const scoreText = document.querySelector('.score-text');
+    const scoreText = document.querySelector(".score-text");
     scoreText.textContent = `Your Score ${totalScore} out of ${questions.length}`;
 }
